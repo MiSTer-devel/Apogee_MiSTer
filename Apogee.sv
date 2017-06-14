ec7175300e5f284bd3694bea6bae5cc66f18d42a
sorgelig <pour.garbage@gmail.com>
2017-06-15 02:07:58 +0800

    Fix the file header.

--- Apogee.sv
+++ Apogee.sv
@@ -1,25 +1,17 @@
-//============================================================================
-// Sinclair ZX Spectrum host board
+// ====================================================================
+//                Vector 06C FPGA REPLICA
+//
+//            Copyright (C) 2016 - 2017 Sorgelig
+//
+// This core is distributed under modified BSD license. 
+// For complete licensing information see LICENSE.TXT.
+// -------------------------------------------------------------------- 
+//
+// An open implementation of Vector 06C home computer
+//
+// Based on code from Dmitry Tselikov and Viacheslav Slavinsky
 // 
-//  Port to DE10-nano board. 
-//  Copyright (C) 2017 Sorgelig
-//
-//  Based on sample ZX Spectrum code by Goran Devic
-//
-//  This program is free software; you can redistribute it and/or modify it
-//  under the terms of the GNU General Public License as published by the Free
-//  Software Foundation; either version 2 of the License, or (at your option)
-//  any later version.
-//
-//  This program is distributed in the hope that it will be useful, but WITHOUT
-//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-//  more details.
-//
-//  You should have received a copy of the GNU General Public License along
-//  with this program; if not, write to the Free Software Foundation, Inc.,
-//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//============================================================================
+
 module emu
 (
 	//Master input clock
